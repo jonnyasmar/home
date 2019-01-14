3f04c91edef44784cefe97e1151653201d042755
Jonny Asmar <jonnyasmar@me.com>
2019-01-14 01:28:59 -0500

    Updates

---
+++ precache-manifest.591032759006965d8babd883b64ecfbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e258ea1747dec9816919",
+    "url": "/home/static/js/runtime~main.e258ea17.js"
+  },
+  {
+    "revision": "b7de8c8a1146bea1b43d",
+    "url": "/home/static/js/main.b7de8c8a.chunk.js"
+  },
+  {
+    "revision": "f7e84482a0cc2f20ebf8",
+    "url": "/home/static/js/1.f7e84482.chunk.js"
+  },
+  {
+    "revision": "b7de8c8a1146bea1b43d",
+    "url": "/home/static/css/main.5879751f.chunk.css"
+  },
+  {
+    "revision": "f19c8f4841d7b7c534c90921fef0d780",
+    "url": "/home/index.html"
+  }
+];
